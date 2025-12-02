f64218d7dc2da702f64507a64ef48893ab2ec751
Felipe Gonçalves <dev.felipegon@gmail.com>
2025-12-01 23:11:59 -0300

    Implementado o beforeEach, verificação dos campos obrigatórios e objeto delay

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,7 +1,49 @@
-describe('Central de Atendimentos CAC TAT', () => {
+describe('Central de Atendimento CAC TAT - Preenchendo formulário', () => {
+  beforeEach(() => {
+    cy.visit('/../../src/index.html')
+  })
+
   it('Verifica o título de aplicação', () => {
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('Verificar título da aplicação', () => {
     cy.visit('/../../src/index.html')
 
-    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    let titulo = cy.title().should('eq','Central de Atendimento ao Cliente TAT')
+
+    if(titulo != true){
+      cy.log('Título da aplicação está correto')
+    }else{
+      cy.log('Título da aplicação está incorreto')
+    }
+  })
+
+  it.only('Preencher os campos obrigatórios e encaminhar o formulário', () => {
+    cy.get('#firstName').should('be.visible').type('Felipe')
+    cy.get('#firstName').should('have.value', 'Felipe')
+
+    cy.get('#lastName').should('be.visible')
+    cy.get('#lastName').type('Augusto')
+    cy.get('#lastName').should('have.value', 'Augusto')
+
+    cy.get('#email').should('be.visible')
+    cy.get('#email').type('teste@gmail.com')
+    cy.get('#email').should('have.value', 'teste@gmail.com')
+
+    const textLong = Cypress._.repeat('Obrigado ', 20)
+
+    cy.get('#open-text-area')
+    .as('desc')
+    cy.get('@desc').should('be.visible')
+    cy.get('@desc').type(textLong, { delay: 0})
+    cy.get('@desc').should('have.value', textLong)
+
+    cy.get('button[type="submit"]')
+    .as('btnEnviar')
+    cy.get('@btnEnviar').should('be.visible')
+    cy.get('@btnEnviar').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
