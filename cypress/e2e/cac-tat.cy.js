8b9ae22faf40affb96f9d755f8b934e194c9a287
Felipe Gonçalves - NúcleoGov <dev.felipegon@gmail.com>
2026-02-04 12:32:58 -0300

    implementação do meu README para documentar

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -255,7 +255,7 @@ describe('Central de Atendimento CAC TAT - Preenchendo formulário', () => {
       
     })
 
-    it.only('Acessa a página da política de privacidade removendo o target e então clicando no link', () =>{
+    it('Acessa a página da política de privacidade removendo o target e então clicando no link', () =>{
       cy.contains('a', 'Política de Privacidade')
         .invoke('removeAttr', 'target')
         .click()
